7e6326af40bcd1a8c47ab860b982af6d7baae4b9
lujin77 <syani@qq.com>
2017-05-28 13:06:24 +0800

    添加注释

--- Python/dp/fibonacci.py
+++ Python/dp/fibonacci.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
+"""斐波拉契数量"""
+
 from functools import wraps
 
 
 def fib(i):
+    """递归方式实现"""
     if i < 2:
         return 1
     return fib(i - 1) + fib(i - 2)
@@ -17,14 +20,17 @@ def memo(func):
         if args not in cache:
             cache[args] = func(*args)
         return cache[args]
+
     return wrap
 
 
 @memo
 def fib2(i):
+    """动态规方式实现，使用decorator缓存中间计算结果"""
     if i < 2:
         return 1
     return fib(i - 1) + fib(i - 2)
+
 
 if __name__ == "__main__":
     print fib(4)
